51e814938b2ee7687447882fbb6a8b6b7a122329
moshe.w@invest.com <1nvest.com>
2016-11-22 15:22:23 +0200

    main activity.

--- android/app/src/main/java/com/github/android/removeduplicates/ItemDetailFragment.java
+++ android/app/src/main/java/com/github/android/removeduplicates/ItemDetailFragment.java
@@ -1,3 +1,20 @@
+/*
+ * Source file of the Remove Duplicates project.
+ * Copyright (c) 2016. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/2.0
+ *
+ * Contributors can be contacted by electronic mail via the project Web pages:
+ *
+ * https://github.com/pnemonic78/RemoveDuplicates
+ *
+ * Contributor(s):
+ *   Moshe Waisberg
+ *
+ */
 package com.github.android.removeduplicates;
 
 import android.app.Activity;
