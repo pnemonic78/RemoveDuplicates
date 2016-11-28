3d2c64005b86999649738b68e191b7b7bddac3f6
moshe.w@invest.com <1nvest.com>
2016-11-28 14:14:55 +0200

    duplicate items.

---
+++ android/app/src/main/java/com/github/duplicates/call/DuplicateCall.java
@@ -0,0 +1,28 @@
+/*
+ * Source file of the Remove Duplicates project.
+ * Copyright (c) 2016. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/2.0
+ *
+ * Contributors can be contacted by electronic mail via the project Web pages:
+ *
+ * https://github.com/pnemonic78/RemoveDuplicates
+ *
+ * Contributor(s):
+ *   Moshe Waisberg
+ *
+ */
+package com.github.duplicates.call;
+
+import com.github.duplicates.DuplicateItem;
+
+/**
+ * Duplicate call log entry.
+ *
+ * @author moshe.w
+ */
+public class DuplicateCall extends DuplicateItem {
+}
