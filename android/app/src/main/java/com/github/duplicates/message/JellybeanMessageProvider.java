880490ee07ffb8057df7703b7d93c37a67f15707
moshe.w@invest.com <1nvest.com>
2016-12-01 16:38:43 +0200

    messages: provider.

--- android/app/src/main/java/com/github/duplicates/message/JellybeanMessageProvider.java
+++ android/app/src/main/java/com/github/duplicates/message/JellybeanMessageProvider.java
@@ -23,6 +23,8 @@ import android.net.Uri;
 
 import com.github.duplicates.DuplicateProvider;
 
+import static android.provider.BaseColumns._ID;
+
 /**
  * Provide duplicate messages for Jellybean versions and older.
  *
@@ -30,7 +32,39 @@ import com.github.duplicates.DuplicateProvider;
  */
 public class JellybeanMessageProvider extends DuplicateProvider<MessageItem> {
 
-    private static final String[] PROJECTION = null;
+    private static final String[] PROJECTION = {
+            _ID,
+            "address",
+            "body",
+            "date",
+            "date_sent",
+            "error_code",
+            "locked",
+            "person",
+            "protocol",
+            "read",
+            "seen",
+            "status",
+            "subject",
+            "thread_id",
+            "type"
+    };
+
+    private static final int INDEX_ID = 0;
+    private static final int INDEX_ADDRESS = 1;
+    private static final int INDEX_BODY = 2;
+    private static final int INDEX_DATE_RECEIVED = 3;
+    private static final int INDEX_DATE_SENT = 4;
+    private static final int INDEX_ERROR_CODE = 5;
+    private static final int INDEX_LOCKED = 6;
+    private static final int INDEX_PERSON = 7;
+    private static final int INDEX_PROTOCOL = 8;
+    private static final int INDEX_READ = 9;
+    private static final int INDEX_SEEN = 10;
+    private static final int INDEX_STATUS = 11;
+    private static final int INDEX_SUBJECT = 12;
+    private static final int INDEX_THREAD_ID = 13;
+    private static final int INDEX_TYPE = 14;
 
     public JellybeanMessageProvider(Context context) {
         super(context);
@@ -53,6 +87,20 @@ public class JellybeanMessageProvider extends DuplicateProvider<MessageItem> {
 
     @Override
     public void populateItem(Cursor cursor, MessageItem item) {
-        //TODO implement me!
+        item.setId(cursor.getLong(INDEX_ID));
+        item.setAddress(cursor.getString(INDEX_ADDRESS));
+        item.setBody(cursor.getString(INDEX_BODY));
+        item.setDateReceived(cursor.getLong(INDEX_DATE_RECEIVED));
+        item.setDateSent(cursor.getLong(INDEX_DATE_SENT));
+        item.setErrorCode(cursor.getInt(INDEX_ERROR_CODE));
+        item.setLocked(cursor.getInt(INDEX_LOCKED) != 0);
+        item.setPerson(cursor.getInt(INDEX_PERSON));
+        item.setProtocol(cursor.getInt(INDEX_PROTOCOL));
+        item.setRead(cursor.getInt(INDEX_READ) != 0);
+        item.setSeen(cursor.getInt(INDEX_SEEN) != 0);
+        item.setStatus(cursor.getInt(INDEX_STATUS));
+        item.setSubject(cursor.getString(INDEX_SUBJECT));
+        item.setThreadId(cursor.getLong(INDEX_THREAD_ID));
+        item.setType(cursor.getInt(INDEX_TYPE));
     }
 }
