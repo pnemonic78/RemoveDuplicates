b1d559d01b33730debe34347de4a11240a0cf368
moshe.w@invest.com <1nvest.com>
2016-12-21 10:51:50 +0200

    default SMS app.

---
+++ android/app/src/main/java/com/github/duplicates/message/MmsReceiver.java
@@ -0,0 +1,34 @@
+/*
+ * Source file of the Remove Duplicates project.
+ * Copyright (c) 2016. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/2.0
+ *
+ * Contributors can be contacted by electronic mail via the project Web pages:
+ *
+ * https://github.com/pnemonic78/RemoveDuplicates
+ *
+ * Contributor(s):
+ *   Moshe Waisberg
+ *
+ */
+package com.github.duplicates.message;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Dummy MMS receiver just to let system think we can be a default SMS app.
+ *
+ * @author moshe.w
+ */
+public class MmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Not an actual receiver.
+    }
+}
