1045fd9aecd923a1bc826f1ac3b03a62a9236683
M. Waisberg <pnemonic@gmail.com>
2019-10-31 11:15:47 +0200

    migrate to kotlin

---
+++ duplicates-android/app/src/main/java/com/github/duplicates/DuplicateItemPair.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016, Moshe Waisberg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.duplicates
+
+import com.github.duplicates.DuplicateComparator.SAME
+
+/**
+ * Item that is a possible duplicate of two items.
+ *
+ * @author moshe.w
+ */
+class DuplicateItemPair<T : DuplicateItem>(@JvmField val item1: T, @JvmField val item2: T, @JvmField val match: Float = 1f, @JvmField val difference: BooleanArray) : Comparable<DuplicateItemPair<T>> {
+
+    val id: Long
+        get() {
+            val id1 = item1.id
+            val id2 = item2.id
+            return id1 and 0xFFFFFFFFL shl 32 or (id2 and 0xFFFFFFFFL)
+        }
+
+    init {
+        if (match >= MATCH_GREAT) {
+            item2.isChecked = true
+        }
+    }
+
+    override fun compareTo(other: DuplicateItemPair<T>): Int {
+        val thisId1 = this.item1.id
+        val thatId1 = other.item1.id
+        val c = thisId1.compareTo(thatId1)
+        if (c != SAME) {
+            return c
+        }
+        val thisId2 = this.item2.id
+        val thatId2 = other.item2.id
+        return thisId2.compareTo(thatId2)
+    }
+
+    companion object {
+
+        /**
+         * Percentage for two items to be considered a very good match.
+         */
+        const val MATCH_GREAT = 0.95f
+    }
+}
