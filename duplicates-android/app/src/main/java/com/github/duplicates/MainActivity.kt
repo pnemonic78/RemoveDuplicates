16cda6d8ffb9aa6f31a606b9e38c110438eeb0cd
M. Waisberg <pnemonic@gmail.com>
2020-06-21 18:44:34 +0300

    migrate Fabric Crashlytics to Firebase Crashlytics.

--- duplicates-android/app/src/main/java/com/github/duplicates/MainActivity.kt
+++ duplicates-android/app/src/main/java/com/github/duplicates/MainActivity.kt
@@ -25,7 +25,6 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
-import com.crashlytics.android.Crashlytics
 import com.github.android.removeduplicates.BuildConfig
 import com.github.android.removeduplicates.R
 import com.github.duplicates.alarm.AlarmDeleteTask
@@ -53,7 +52,6 @@ import com.github.duplicates.message.MessageFindTask
 import com.github.duplicates.message.MessageItem
 import com.github.duplicates.message.MessageViewHolder
 import com.github.util.LogTree
-import io.fabric.sdk.android.Fabric
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
@@ -75,9 +73,6 @@ class MainActivity<I : DuplicateItem, T : DuplicateTask<I, *, *, *, DuplicateTas
         super.onCreate(savedInstanceState)
 
         Timber.plant(LogTree(BuildConfig.DEBUG))
-        if (!BuildConfig.DEBUG) {
-            Fabric.with(this, Crashlytics())
-        }
 
         setContentView(R.layout.activity_main)
 
