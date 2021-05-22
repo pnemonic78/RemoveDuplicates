577b726264e054b6581f9ec1658562eda452e3e1
M. Waisberg <pnemonic@gmail.com>
2021-05-23 00:46:51 +0300

    compile SDK 30

--- duplicates-android/buildSrc/src/main/java/BuildVersions.kt
+++ duplicates-android/buildSrc/src/main/java/BuildVersions.kt
@@ -2,7 +2,7 @@ object BuildVersions {
     const val kotlin_version = "1.4.32"
 
     const val minSdkVersion = 19
-    const val compileSdkVersion = 29
+    const val compileSdkVersion = 30
     const val targetSdkVersion = 29
 
     // App dependencies
